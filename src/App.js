84b2b516f1f8cbfcb9839d65e1a529a5c158589c
AnishAB2 <anish@ab2consulting.com>
2020-03-24 10:24:45 +0530

    Count task using for usestate

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-import React, { Component } from 'react';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      clicks: 0,
-      show: true
-    };
-  }
-
-  IncrementItem = () => {
-    this.setState({ clicks: this.state.clicks + 1 });
-  }
-  DecreaseItem = () => {
-    this.setState({ clicks: this.state.clicks - 1 });
-  }
-  ToggleClick = () => {
-    this.setState({ show: !this.state.show });
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.IncrementItem}>Click to increment by 1</button>
-        <button onClick={this.DecreaseItem}>Click to decrease by 1</button>
-        <button onClick={this.ToggleClick}>
-          { this.state.show ? 'Hide number' : 'Show number' }
-        </button>
-        { this.state.show ? <h2>{ this.state.clicks }</h2> : '' }
-      </div>
-    );
-  }
+import React, { useState } from 'react';
+const Count=()=>{
+  const[click,setCount]=useState(0);
+  return(
+    <div>  
+      <p>Current count is "{click}"</p>
+    <button onClick={()=>setCount(click+1)}>Click to increment by 1</button>
+    <button onClick={()=>setCount(click-1)}>Click to decrease by 1</button>   
+  </div>
+  );
 }
 
-export default App;
+
+export default Count;
